2c3538148493fd418535b0bff33d5eb2154bf5f4
Lisa Vinson <Lrv0828@gmail.com>
2017-11-21 01:24:24 -0500

    Created html, css, and javascript file, added bootstrap layout, added object with questions and answers (first draft)

---
+++ assets/javascript/app.js
@@ -0,0 +1,139 @@
+//	Page loads (ready on document)
+// Music starts playing (Fresh Prince theme music?)
+// User presses button to begin
+
+//Object with 1 key for each possible question (12)
+    var triviaQuestions = {
+        
+        "question1": { 
+            "question": "The American comedy ran for a how many seasons?",
+            "answer": "six seasons and 148 episodes",
+            "wrongChoice1": "tbd",
+            "wrongChoice2": "tbd",
+            "wrongChoice3": "tbd",
+            "display": "tbd",
+            "extraFact": " Something about awards won?"
+            
+        },
+
+        "question2": {  
+            "question": "When Ashley reveals to Will that she does not enjoy taking violin lessons, he pawns her violin in for a: ",
+            "answer": "drum set",
+            "wrongChoice1":  "keyboard",
+            "wrongChoice2": "trumpet",
+            "wrongChoice3": "turn table",
+            "display": "Video clip of Jazz playing, will dancing",
+            "extraFact": "Will hired his friend Jazz, played by DJ Jazzy Jeff to teach Ashley how to play her new drum set."
+        },
+
+        "question3": { 
+            "question": "While visiting, Uncle Phil’s mom, Hattie **, reveals to Will over a game of cards that Uncle Phil’s nickname back in his country hometown is:",
+            "answer": "Zeke",
+            "wrongChoice1": "Philly",
+            "wrongChoice2": "Big Phil",
+            "wrongChoice3": "Sonny",
+            "display": "tbd",
+            extraFact:  "tbd:"
+        },
+
+        •	Question4:
+        o	question: Aiming to impress the girls in his poetry class, Will spontaneously make up a poem called “Tick Tock Clock” that he claims is written by a poet friend named:
+        o	answer:  Raphael de la Ghetto
+        o	wrongChoice1: Jazzy Jeff
+        o	wrongChoice2:
+        o	wrongChoice3:
+        o	Image/Video: Picture of Jeoffery as Rafael de la Ghetto
+        o	extraFact:  
+        
+        •	Question5: 
+        o	question: When Will and Carlton find out that Hilary dropped out of college, they blackmail her into the following humiliating task:
+        o	answer:  All of the above
+        o	wrongChoice1:   Complimenting Will whenever Uncle Phil takes a drink of water
+        o	wrongChoice2:   Barking like a dog when someone says her name
+        o	wrongChoice3:  Hitting Will when Carlton clears his throat
+        o	WrongChoice4: Insulting Will when Carlton hits his glass
+        o	Image/Video: This scene from the show
+        o	extraFact:  
+        
+        
+        •	Question6:
+        o	question:  When Ashley is being bullied at school, Will and Carlton try to help by:
+        o	answer:  bribing the bully with $50
+        o	wrongChoice1:  telling a teacher
+        o	wrongChoice2: switching Ashley’s classes
+        o	wrongChoice3: taking the bully on a date
+        o	Image/Video: Scene of Will teaching Ashley how to fight
+        o	extraFact:  
+        
+        •	Question7:
+        o	Question: Nervous over turning 40, Aunt Vivian enrolls in this class, surprising her much younger class mates with a star performance.
+        o	answer: dance class
+        o	wrongChoice1: singing class
+        o	wrongChoice2: acting class
+        o	wrongChoice3: modeling class
+        o	Image/Video: Extended gif/Video of Aunt Viv’s dance scene
+        o	extraFact:  Before becoming the “original” Aunt Viv for the first three seasons of Fresh Prince, Janet Huber studied at Julliard School and the Alvin Ailey Dance company. 
+        
+        •	Question8: Suing Uncle Phil for Will crashing into his car, Hilary’s boyfriend Eric, played by ***, admits admit he didn’t have this with him during the accident:
+        o	answer: his glasses
+        o	wrongChoice1:  his license
+        o	wrongChoice2: his registration
+        o	wrongChoice3: 
+        o	Image/Video: 
+        o	extraFact:  
+        
+        
+        •	Question9: When Will wins $1,000 at a charity fundraiser, he refuses to donate the money to the designated charity and instead does what with his winnings?
+        o	answer:  Donates it to his friend Ramon who wanted to afford a way to basketball camp
+        o	wrongChoice1: spends it on new speakers
+        o	wrongChoice2: spends it on new shoes
+        o	wrongChoice3: gives it to Ashley for singing lessons
+        o	Image/Video: 
+        o	extraFact:  
+        o	
+        
+        •	Question10:
+        o	Question: When a Princeton recruiter comes to Will and Carl’s school for interviews, Will impresses the recruiter by doing what?
+        o	answer: Solving a Rubik’s cube
+        o	wrongChoice1:  Reciting a poem
+        o	wrongChoice2: Solving a complex math problem
+        o	wrongChoice3: winning a debate with another student
+        o	Image/Video: 
+        o	extraFact:  In the film Pursuit of Happyness, ****, played by Will Smith, impresses a top executive of a finance firm he would later interview for by solving a rubiks cube, which the actor can do in real life.
+        
+        •	Question 11: 
+        o	question: Worried that it might damage Uncle Phil’s reputation, Geoffrey confesses that he did this, causing him to hide by moving to the U.S.
+        o	answer:  Cheating in an Olympic race by taking a taxi to the finish line
+        o	wrongChoice1:  stealing from a previous employer
+        o	wrongChoice2: 
+        o	wrongChoice3:
+        o	Image/Video: 
+        o	extraFact:  
+        
+        •	Question 12: 
+        o	question: Upset that she is being treated by a child by Uncle Phil and Will, Ashley performs a version of this song in front of her family
+        o	answer: “Respect” by Aretha Franklin
+        o	wrongChoice1: 
+        o	wrongChoice2:
+        o	wrongChoice3:
+        o	Image/Video: 
+        o	extraFact:  
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
