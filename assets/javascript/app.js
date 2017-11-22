dc3bc95b63a65d823ebaee7a1c546c163f020f86
Lisa Vinson <Lrv0828@gmail.com>
2017-11-22 01:15:26 -0500

    Updated trivia object, added js for instructions and question panels to alternate hide/show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,138 +2,127 @@
 // Music starts playing (Fresh Prince theme music?)
 // User presses button to begin
 
-//Object with 1 key for each possible question (12)
+//-----GLOBAL VARIABLES ----------------------------------------------
+
+    var themeSong = "tbd";//Create audio element, will assign src to theme song mp3 file
+
+    var gameStarted = false;
+
     var triviaQuestions = {
-        
-        "question1": { 
-            "question": "The American comedy ran for a how many seasons?",
-            "answer": "six seasons and 148 episodes",
-            "wrongChoice1": "tbd",
-            "wrongChoice2": "tbd",
-            "wrongChoice3": "tbd",
-            "display": "tbd",
-            "extraFact": " Something about awards won?"
-            
+
+        question1: {  
+            question: "When Ashley reveals to Will that she does not enjoy taking violin lessons, he pawns her violin in for a: ",
+            answer: "drum set",
+            wrongChoices: ["wrongChoice1", "wrongChoice2", "wrongChoice3"],
+            display: "tbd",  //"Video clip of Jazz playing, will dancing",
+            extraFact: "Will hired his friend Jazz, played by DJ Jazzy Jeff to teach Ashley how to play her new drum set"
         },
 
-        "question2": {  
-            "question": "When Ashley reveals to Will that she does not enjoy taking violin lessons, he pawns her violin in for a: ",
-            "answer": "drum set",
-            "wrongChoice1":  "keyboard",
-            "wrongChoice2": "trumpet",
-            "wrongChoice3": "turn table",
-            "display": "Video clip of Jazz playing, will dancing",
-            "extraFact": "Will hired his friend Jazz, played by DJ Jazzy Jeff to teach Ashley how to play her new drum set."
+        question2: { 
+            question: "While visiting, Uncle Phil’s mom reveals to Will over a game of cards that Uncle Phil’s nickname back in his country hometown is:",
+            answer: "Zeke",
+            wrongChoices: ["Philly", "Big Phil", "Sonny"],
+            display: "tbd",  //"TBD",
+            extraFact: "TBD"
+
         },
 
-        "question3": { 
-            "question": "While visiting, Uncle Phil’s mom, Hattie **, reveals to Will over a game of cards that Uncle Phil’s nickname back in his country hometown is:",
-            "answer": "Zeke",
-            "wrongChoice1": "Philly",
-            "wrongChoice2": "Big Phil",
-            "wrongChoice3": "Sonny",
-            "display": "tbd",
-            extraFact:  "tbd:"
+        question3: {
+            question: "Aiming to impress the girls in his poetry class, Will spontaneously makes up a poem called 'Tick Tock Clock' that he claims is written by a poet friend named:",
+            answer: " Raphael de la Ghetto",
+            wrongChoices: ["Jazzy Jeff", "TBD", "TBD"],
+            display: "TBD",  // Picture of Jeoffery as Rafael de la Ghetto
+            extraFact: "TBD"
+        
+        },
+  
+        question4: {
+            question: "When Ashley is being bullied at school, Will and Carlton try to help by:",
+            answer: "bribing the bully with $50",
+            wrongChoices: ["telling a teacher", "switching Ashley’s class", "taking the bully on a date"],
+            display: "TBD",  // Scene of Will teaching Ashley how to figh
+            extraFact: "TBD"
+        },
+        
+        question5: {
+            question: "Nervous about turning 40, Aunt Vivian enrolls in this type of class, surprising her much younger classmates with a star performance:",
+            answer: "dance class",
+            wrongChoices: ["singing lessons", "acting class", " modeling class"],
+            display: "TBD",  // Extended gif/Video of Aunt Viv’s dance scene
+            extraFact: "Before being cast as Aunt Viv, Janet Huber studied at Julliard School and the Alvin Ailey Dance company"
         },
 
-        •	Question4:
-        o	question: Aiming to impress the girls in his poetry class, Will spontaneously make up a poem called “Tick Tock Clock” that he claims is written by a poet friend named:
-        o	answer:  Raphael de la Ghetto
-        o	wrongChoice1: Jazzy Jeff
-        o	wrongChoice2:
-        o	wrongChoice3:
-        o	Image/Video: Picture of Jeoffery as Rafael de la Ghetto
-        o	extraFact:  
-        
-        •	Question5: 
-        o	question: When Will and Carlton find out that Hilary dropped out of college, they blackmail her into the following humiliating task:
-        o	answer:  All of the above
-        o	wrongChoice1:   Complimenting Will whenever Uncle Phil takes a drink of water
-        o	wrongChoice2:   Barking like a dog when someone says her name
-        o	wrongChoice3:  Hitting Will when Carlton clears his throat
-        o	WrongChoice4: Insulting Will when Carlton hits his glass
-        o	Image/Video: This scene from the show
-        o	extraFact:  
-        
-        
-        •	Question6:
-        o	question:  When Ashley is being bullied at school, Will and Carlton try to help by:
-        o	answer:  bribing the bully with $50
-        o	wrongChoice1:  telling a teacher
-        o	wrongChoice2: switching Ashley’s classes
-        o	wrongChoice3: taking the bully on a date
-        o	Image/Video: Scene of Will teaching Ashley how to fight
-        o	extraFact:  
-        
-        •	Question7:
-        o	Question: Nervous over turning 40, Aunt Vivian enrolls in this class, surprising her much younger class mates with a star performance.
-        o	answer: dance class
-        o	wrongChoice1: singing class
-        o	wrongChoice2: acting class
-        o	wrongChoice3: modeling class
-        o	Image/Video: Extended gif/Video of Aunt Viv’s dance scene
-        o	extraFact:  Before becoming the “original” Aunt Viv for the first three seasons of Fresh Prince, Janet Huber studied at Julliard School and the Alvin Ailey Dance company. 
-        
-        •	Question8: Suing Uncle Phil for Will crashing into his car, Hilary’s boyfriend Eric, played by ***, admits admit he didn’t have this with him during the accident:
-        o	answer: his glasses
-        o	wrongChoice1:  his license
-        o	wrongChoice2: his registration
-        o	wrongChoice3: 
-        o	Image/Video: 
-        o	extraFact:  
-        
-        
-        •	Question9: When Will wins $1,000 at a charity fundraiser, he refuses to donate the money to the designated charity and instead does what with his winnings?
-        o	answer:  Donates it to his friend Ramon who wanted to afford a way to basketball camp
-        o	wrongChoice1: spends it on new speakers
-        o	wrongChoice2: spends it on new shoes
-        o	wrongChoice3: gives it to Ashley for singing lessons
-        o	Image/Video: 
-        o	extraFact:  
-        o	
-        
-        •	Question10:
-        o	Question: When a Princeton recruiter comes to Will and Carl’s school for interviews, Will impresses the recruiter by doing what?
-        o	answer: Solving a Rubik’s cube
-        o	wrongChoice1:  Reciting a poem
-        o	wrongChoice2: Solving a complex math problem
-        o	wrongChoice3: winning a debate with another student
-        o	Image/Video: 
-        o	extraFact:  In the film Pursuit of Happyness, ****, played by Will Smith, impresses a top executive of a finance firm he would later interview for by solving a rubiks cube, which the actor can do in real life.
-        
-        •	Question 11: 
-        o	question: Worried that it might damage Uncle Phil’s reputation, Geoffrey confesses that he did this, causing him to hide by moving to the U.S.
-        o	answer:  Cheating in an Olympic race by taking a taxi to the finish line
-        o	wrongChoice1:  stealing from a previous employer
-        o	wrongChoice2: 
-        o	wrongChoice3:
-        o	Image/Video: 
-        o	extraFact:  
-        
-        •	Question 12: 
-        o	question: Upset that she is being treated by a child by Uncle Phil and Will, Ashley performs a version of this song in front of her family
-        o	answer: “Respect” by Aretha Franklin
-        o	wrongChoice1: 
-        o	wrongChoice2:
-        o	wrongChoice3:
-        o	Image/Video: 
-        o	extraFact:  
-        
+        question6: {
+            question: "Suing Uncle Phil for Will crashing into his car, Hilary’s boyfriend Eric, admits he didn’t have his this with him during the accident:",
+            answer: "glasses",
+            wrongChoices: ["license", "registration", "wallet"],
+            display: "TBD",  // 
+            extraFact: "TBD"
+        },
 
+        question7: {
+            question: "When Will wins $1,000 at a charity fundraiser, he refuses to donate the money to the designated charity and instead does what with his winnings?",
+            answer: "Donates it to his friend Ramon who wanted to afford a way to basketball camp",
+            wrongChoices: ["buys new speakers", "buys new shoes", "gives it to Ashley for singing lessons"],
+            display: "TBD",  // 
+            extraFact: "TBD"
+        },
 
+        question8: {
+            question: "When a Princeton recruiter comes to Will and Carl’s school for interviews, Will impresses the recruiter by doing what?",
+            answer: "Solving a Rubik’s cube",
+            wrongChoices: ["Reciting a rap", "Solving a complex math problem", "winning a debate with another student"],
+            display: "TBD",  // 
+            extraFact: "In the film Pursuit of Happyness, Will Smith plays a character that solves a rubiks cube and impresses a top executive who later hires him, a feat the actor can do in real life."
+        },
+  
+        question9: {
+            question: "Worried that it might damage Uncle Phil’s reputation during his campaign for judge, Geoffrey confesses that he did this, causing him to go into hiding by moving to the U.S.",
+            answer: "Cheating in an Olympic race by taking a taxi to the finish line",
+            wrongChoices: ["stealing from a previous employer", "TBD", "TBD"],
+            display: "TBD",  // 
+            extraFact: "TBD"
+        },
 
-
-
-
-
-
-
-
-
-
+        question10: {
+            question: "Upset that she is being treated like a child, Ashley performs a version of this song on stage at a karoke event in front of her family:",
+            answer: "Cheating in an Olympic race by taking a taxi to the finish line",
+            wrongChoices: ["'Respect' by Aretha Franklin", "TBD", "TBD"],
+            display: "TBD",  // 
+            extraFact: "TBD"
+        },
 
     }
 
+    function startGame(){
+        console.log("Start game function!")
+    }
 
+
+    //When the page loads:
+    $(document).ready(function(){
+        $("#gamePanel").hide();
+            //Set the song to play
+        // $(themeSong).attr("src", "assets/images/themeSongFull.mp3");
+        // themeSong.play();
+
+            //Select start button and when clicked run an anonymous callback function
+        $("#startButton").on("click",function(){
+            gameStarted = true;
+            console.log(startGame);
+
+            if (gameStarted){
+                $("#instructionsPanel").hide();
+                $("#gamePanel").show();
+                startGame();        
+                }
+    
+        });
+
+
+
+       
+            
+    })
 
 
